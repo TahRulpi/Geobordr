462cd61fb32954f04c21c882879cb70a49a6f101
Tahlamuna Rulpi <tahrulpi97@gmail.com>
2025-08-21 13:46:39 +0600

    obj

---
+++ Assets/Scripts/CountryObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; 
+
+public class CountryObject : MonoBehaviour
+{
+   
+    public CountryData countryData;
+
+    // Reference to a Text element if you want to display the name.
+    public Text nameText;
+
+    private void Start()
+    {
+        // This is a simple example. In a real game, you might do this differently.
+        if (countryData != null)
+        {
+            // Set the sprite of the GameObject to match the one in the Scriptable Object.
+            // (This is redundant if you dragged the sprite directly, but good practice).
+            GetComponent<SpriteRenderer>().sprite = countryData.countrySprite;
+
+            // If you have a Text component, update it with the country name.
+            if (nameText != null)
+            {
+                nameText.text = countryData.countryName;
+            }
+        }
+    }
+
+    // Example of a function that could be called when the player clicks on the country.
+    void OnMouseDown()
+    {
+        if (countryData != null)
+        {
+            Debug.Log("Clicked on " + countryData.countryName);
+           
+        }
+    }
+}
